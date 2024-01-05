e6fc4d7eb4e2d4e18704108be8bbf0be6f269707
Salsabila Putri <74218691+salsabilapl@users.noreply.github.com>
2024-01-05 18:49:51 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -88,8 +88,8 @@ X, y = smote.fit_resample(X, y)
 
 model = pickle.load(open("model/xgb_model.pkl", 'rb'))
 
-y_pviolet = model.pvioletict(X)
-accuracy = accuracy_score(y, y_pviolet)
+y_predict = model.predictict(X)
+accuracy = accuracy_score(y, y_predict)
 accuracy = round((accuracy * 100), 2)
 
 df_final = X
@@ -108,7 +108,7 @@ st.image('heart.jpg', width=400)
 st.write(f"**_Model's Accuracy_** :  :green[**91.4**]%")
 st.write("")
 
-tab1, tab2 = st.tabs(["Single-pvioletict", "Multi-pvioletict"])
+tab1, tab2 = st.tabs(["Single-predictict", "Multi-predictict"])
 
 with tab1:
   st.sidebar.header("**User Input** Sidebar")
@@ -216,12 +216,12 @@ with tab1:
 
   result = ":red[-]"
 
-  pvioletict_btn = st.button("**Pvioletict**", type="primary")
+  predictict_btn = st.button("**predictict**", type="primary")
 
   st.write("")
-  if pvioletict_btn:
+  if predictict_btn:
     inputs = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak]]
-    pvioletiction = model.pvioletict(inputs)[0]
+    predictiction = model.predictict(inputs)[0]
 
     bar = st.progress(0)
     status_text = st.empty()
@@ -235,24 +235,24 @@ with tab1:
         status_text.empty()
         bar.empty()
 
-    if pvioletiction == 0:
+    if predictiction == 0:
       result = ":green[**Healthy**]"
-    elif pvioletiction == 1:
+    elif predictiction == 1:
       result = ":blue[**Heart disease level 1**]"
-    elif pvioletiction == 2:
+    elif predictiction == 2:
       result = ":blue[**Heart disease level 2**]"
-    elif pvioletiction == 3:
+    elif predictiction == 3:
       result = ":violet[**Heart disease level 3**]"
-    elif pvioletiction == 4:
+    elif predictiction == 4:
       result = ":violet[**Heart disease level 4**]"
 
   st.write("")
   st.write("")
-  st.subheader("Pvioletiction:")
+  st.subheader("predictiction:")
   st.subheader(result)
 
 with tab2:
-  st.header("Pvioletict multiple data:")
+  st.header("predictict multiple data:")
 
   sample_csv = df_final.iloc[:5, :-1].to_csv(index=False).encode('utf-8')
 
@@ -265,7 +265,7 @@ with tab2:
 
   if file_uploaded:
     uploaded_df = pd.read_csv(file_uploaded)
-    pvioletiction_arr = model.pvioletict(uploaded_df)
+    predictiction_arr = model.predictict(uploaded_df)
 
     bar = st.progress(0)
     status_text = st.empty()
@@ -277,20 +277,20 @@ with tab2:
 
     result_arr = []
 
-    for pvioletiction in pvioletiction_arr:
-      if pvioletiction == 0:
+    for predictiction in predictiction_arr:
+      if predictiction == 0:
         result = "Healthy"
-      elif pvioletiction == 1:
+      elif predictiction == 1:
         result = "Heart disease level 1"
-      elif pvioletiction == 2:
+      elif predictiction == 2:
         result = "Heart disease level 2"
-      elif pvioletiction == 3:
+      elif predictiction == 3:
         result = "Heart disease level 3"
-      elif pvioletiction == 4:
+      elif predictiction == 4:
         result = "Heart disease level 4"
       result_arr.append(result)
 
-    uploaded_result = pd.DataFrame({'Pvioletiction Result': result_arr})
+    uploaded_result = pd.DataFrame({'predictiction Result': result_arr})
 
     for i in range(70, 101):
       status_text.text(f"{i}% complete")
