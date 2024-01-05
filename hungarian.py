9b3937d204f3e439a00a0cfdba71dda1173ec87f
Salsabila Putri <74218691+salsabilapl@users.noreply.github.com>
2024-01-05 18:01:33 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 import streamlit as st
 import time
 import pickle
+import joblib
 
 with open("data/hungarian.data", encoding='Latin1') as file:
   lines = [line.strip() for line in file]
@@ -86,8 +87,9 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-model = pickle.load(open("model/xgb_model.pkl", 'rb'))
-
+#model = pickle.load(open("model/xgb_model.pkl", 'rb'))
+model = joblib.load("model/xgb_model.pkl")
+  
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
 accuracy = round((accuracy * 100), 2)
